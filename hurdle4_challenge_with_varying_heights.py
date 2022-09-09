8aceb93b329dc3c218e77c27e4b93727a6c49a75
azi08 <78962131+azi08@users.noreply.github.com>
2022-09-09 11:57:01 +0800

    Update hurdle4_challenge_with_varying_heights.py

--- hurdle4_challenge_with_varying_heights.py
+++ hurdle4_challenge_with_varying_heights.py
@@ -23,3 +23,32 @@ while not at_goal():
         move()  
     else:
         jump()
+        
+       
+ ###########OR###############
+
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+def jump():
+    x = 0
+    turn_left()
+    while wall_on_right():
+        move()
+        x += 1
+    else:
+        turn_right()
+        move()
+        turn_right()
+        while x > 0:
+            move()
+            x -= 1
+    turn_left()
+    
+while not at_goal():    
+    if front_is_clear():
+        move()  
+    else:
+        jump()
